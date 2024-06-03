6a83606cee49a8199acbd52ce76af021bfa87aeb
Anirudh <anirudh.panchangamranganath@thws.study.de>
2024-06-03 10:56:16 +0200

    :wrench: Fix theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <Toaster />
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
